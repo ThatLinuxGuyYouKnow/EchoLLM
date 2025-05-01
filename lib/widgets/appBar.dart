2fed925e83015d9f4f8003b14d10b6dbb02fe2f1
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-01 23:31:57 +0100

    feature: model seletor

--- lib/widgets/appBar.dart
+++ lib/widgets/appBar.dart
@@ -1,9 +1,16 @@
+import 'package:echo_llm/widgets/modelSelector.dart';
 import 'package:flutter/material.dart';
 
 class DarkAppBar extends StatelessWidget implements PreferredSizeWidget {
   DarkAppBar({super.key});
   Widget build(BuildContext context) {
     return AppBar(
+      actions: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Modelselector(),
+        )
+      ],
       backgroundColor: Colors.black,
       title: Text(
         'EchoLLM',
@@ -12,5 +19,5 @@ class DarkAppBar extends StatelessWidget implements PreferredSizeWidget {
     );
   }
 
-  Size get preferredSize => const Size.fromHeight(50);
+  Size get preferredSize => const Size.fromHeight(70);
 }
---
+++ lib/widgets/modelSelector.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Modelselector extends StatefulWidget {
+  Modelselector({super.key});
+
+  @override
+  State<Modelselector> createState() => _ModelselectorState();
+}
+
+class _ModelselectorState extends State<Modelselector> {
+  String selectedValue = 'Agege GPT v1';
+
+  List<String> options = ['Agege GPT v1', 'Agege GPT v2', 'Gemini 2.5 pro'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(color: Colors.cyan)),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<String>(
+          value: selectedValue,
+          isExpanded: true,
+          icon: Icon(
+            Icons.keyboard_arrow_down,
+            color: Colors.white70,
+          ),
+          dropdownColor: Color(0xFF2A3441),
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+          items: options.map((option) {
+            return DropdownMenuItem<String>(
+              value: option,
+              child: Text(option),
+            );
+          }).toList(),
+          onChanged: (value) {
+            if (value != null) {
+              setState(() {
+                selectedValue = value;
+              });
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
