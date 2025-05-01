b9e5c46b5d2f74a5c8dc8fc0e7c730dca60ba62f
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-02 00:27:36 +0100

    feature: expanding when entered text crosses threshold

--- lib/widgets/textfield.dart
+++ lib/widgets/textfield.dart
@@ -9,44 +9,67 @@ class ChatTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final sizeProvider = Provider.of<Textfieldstate>(context);
-
-    return Container(
-      height: sizeProvider.isExpanded ? 400 : 100,
-      width: double.infinity * .75,
-      decoration: BoxDecoration(
-        color: Colors.black,
-        border: Border(top: BorderSide(color: Colors.cyan)),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Color(0xFF1E2733),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: TextField(
-                  onChanged: (text) {
-                    if (text.length > 100) sizeProvider.expand();
-                  },
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.white),
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Type a message',
-                    contentPadding:
-                        const EdgeInsets.symmetric(horizontal: 12.0),
-                    hintStyle: const TextStyle(fontWeight: FontWeight.normal),
+    final textfieldState = Provider.of<Textfieldstate>(context);
+    bool isExpanded = textfieldState.isExpanded;
+    return AnimatedSize(
+      duration: const Duration(milliseconds: 200),
+      curve: Curves.easeOut,
+      child: Container(
+        constraints: BoxConstraints(
+          minHeight: 100,
+          maxHeight: isExpanded ? 400 : 150,
+        ),
+        width: double.infinity * .75,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          border: Border(top: BorderSide(color: Colors.cyan)),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+          child: Column(
+            children: [
+              // TextField
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF1E2733),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    scrollPadding: EdgeInsets.only(top: isExpanded ? 20 : 0),
+                    controller: chatController,
+                    maxLines: isExpanded ? 8 : 1,
+                    minLines: 1,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Type a message',
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 12.0),
+                      hintStyle: const TextStyle(fontWeight: FontWeight.normal),
+                    ),
+                    onChanged: (text) {
+                      if (text.length > 100 && !isExpanded) {
+                        textfieldState.expand();
+                      } else if (text.length < 50 &&
+                          textfieldState.isExpanded) {
+                        textfieldState.minimize();
+                      }
+                    },
                   ),
                 ),
               ),
-            ),
-            const SizedBox(width: 12),
-            ChatButton(),
-          ],
+              const SizedBox(height: 8),
+              // Button Row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ChatButton(),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
