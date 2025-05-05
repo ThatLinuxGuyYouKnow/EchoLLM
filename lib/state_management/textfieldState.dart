4babdfbbd3878302094831c337fd5b2a879c83da
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-05 18:08:40 +0100

    feature: added state for textfield visibility

--- lib/state_management/textfieldState.dart
+++ lib/state_management/textfieldState.dart
@@ -3,6 +3,17 @@ import 'package:flutter/material.dart';
 class Textfieldstate extends ChangeNotifier {
   bool _isExpanded = false;
   bool get isExpanded => _isExpanded;
+  bool get isVisible => _isVisible;
+  bool _isVisible = true;
+  makeVisible() {
+    _isVisible = true;
+    notifyListeners();
+  }
+
+  makeInvisible() {
+    _isVisible = false;
+    notifyListeners();
+  }
 
   expand() {
     _isExpanded = true;
