f4f08bfcc5045318f863483a33ffa459aeaf608f
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-08 03:11:39 +0100

    chore: model screen for displaying model options

---
+++ lib/screens/modelScreen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ModelScreen extends StatelessWidget {
+  ModelScreen({super.key});
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
