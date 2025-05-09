10d851253429a55a06f480b234cd04e448c61722
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-09 01:27:21 +0100

    feature: adapted sidebar to screen state

--- lib/widgets/sidebar.dart
+++ lib/widgets/sidebar.dart
@@ -12,6 +12,7 @@ class CustomSideBar extends StatelessWidget {
     final sidebar = Provider.of<Sidebarstate>(context);
     final messageState = Provider.of<Messagestreamstate>(context);
     final setScreenTo = Provider.of<Screenstate>(context);
+    final isOnChatScreen = Provider.of<Screenstate>(context).isOnMainScreen;
     return Material(
       child: AnimatedContainer(
         color: Color(0xFF1E2733),
@@ -61,7 +62,16 @@ class CustomSideBar extends StatelessWidget {
                 tileTitle: 'Keys',
                 tileIcon: Icons.key,
                 onTilePressed: () {},
-              )
+              ),
+              SizedBox(
+                height: 70,
+              ),
+              isOnChatScreen
+                  ? SizedBox.shrink()
+                  : SpecialDrawerTile(
+                      tileTitle: 'Back to chat',
+                      tileIcon: Icons.arrow_back_ios,
+                      onTilePressed: () {})
             ],
           ),
         ),
@@ -126,6 +136,73 @@ class _DrawerTileState extends State<DrawerTile> {
                   Icon(
                     widget.tileIcon,
                     color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SpecialDrawerTile extends StatefulWidget {
+  final String tileTitle;
+  final IconData tileIcon;
+  final Function() onTilePressed;
+  SpecialDrawerTile(
+      {super.key,
+      required this.tileTitle,
+      required this.tileIcon,
+      required this.onTilePressed});
+
+  @override
+  State<SpecialDrawerTile> createState() => _SpecialDrawerTileState();
+}
+
+class _SpecialDrawerTileState extends State<SpecialDrawerTile> {
+  bool isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: MouseRegion(
+        onHover: (event) {
+          isHovered = true;
+          setState(() {});
+        },
+        onExit: (event) {
+          isHovered = false;
+          setState(() {});
+        },
+        child: GestureDetector(
+          onTap: () {
+            widget.onTilePressed();
+          },
+          child: Container(
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(isHovered ? 0.2 : 0.7),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    widget.tileIcon,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    widget.tileTitle,
+                    style: GoogleFonts.ubuntu(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
                   ),
                 ],
               ),
