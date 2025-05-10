1aefb6e8e732170619eeb7e16d5d5d8ed0a30f66
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-11 00:35:58 +0100

    feature: api key modal is prettier

--- lib/widgets/modals/enterApiKeyModal.dart
+++ lib/widgets/modals/enterApiKeyModal.dart
@@ -18,20 +18,19 @@ class EnterApiKeyModal extends StatelessWidget {
       autofocus: true,
       focusNode: focusNode,
       onKey: (node, event) {
-        if (event.logicalKey == LogicalKeyboardKey.escape ||
-            event.runtimeType.toString() == 'KeyDownEvent') {
+        if (event.logicalKey == LogicalKeyboardKey.escape &&
+            event is RawKeyDownEvent) {
           modalState.setModalToHidden();
           return KeyEventResult.handled;
         }
         return KeyEventResult.ignored;
       },
       child: Container(
-        height: 200,
-        width: 600,
-        padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 28),
+        width: 500,
+        padding: const EdgeInsets.all(24),
         decoration: BoxDecoration(
           color: const Color(0xFF1E2733),
-          borderRadius: BorderRadius.circular(10),
+          borderRadius: BorderRadius.circular(12),
         ),
         child: Column(
           mainAxisSize: MainAxisSize.min,
@@ -42,33 +41,77 @@ class EnterApiKeyModal extends StatelessWidget {
                 'Enter your API key for $modelName',
                 style: GoogleFonts.ubuntu(
                   color: Colors.white,
-                  fontSize: 18,
+                  fontSize: 17,
                   fontWeight: FontWeight.w500,
                 ),
               ),
             ),
-            const SizedBox(height: 30),
+            const SizedBox(height: 20),
             Container(
-              height: 60,
+              height: 50,
               decoration: BoxDecoration(
                 color: const Color.fromARGB(255, 26, 31, 37),
-                borderRadius: BorderRadius.circular(10),
+                borderRadius: BorderRadius.circular(8),
               ),
-              child: Center(
-                child: TextField(
-                  style: TextStyle(color: Colors.grey[50]),
-                  cursorColor: Colors.white70,
-                  decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.symmetric(horizontal: 12),
-                    border: InputBorder.none,
-                    hintText: 'Paste your API key here...',
-                    hintStyle: const TextStyle(color: Colors.white38),
-                  ),
+              child: TextField(
+                style: TextStyle(color: Colors.grey[50]),
+                cursorColor: Colors.white70,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 14),
+                  border: InputBorder.none,
+                  hintText: 'Paste your API key here...',
+                  hintStyle: const TextStyle(color: Colors.white38),
                 ),
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 30),
+            Row(
+              children: [
+                Spacer(),
+                ModalButton(buttonText: 'Save'),
+              ],
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ModalButton extends StatefulWidget {
+  final String buttonText;
+  const ModalButton({required this.buttonText, super.key});
+
+  @override
+  State<ModalButton> createState() => _ModalButtonState();
+}
+
+class _ModalButtonState extends State<ModalButton> {
+  bool isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) => setState(() => isHovered = true),
+      onExit: (_) => setState(() => isHovered = false),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 150),
+        height: 40,
+        width: 110,
+        decoration: BoxDecoration(
+          color: isHovered
+              ? const Color(0xFF4C83D1)
+              : const Color(0xFF3F72AF), // subtle hover shade
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Text(
+            widget.buttonText,
+            style: GoogleFonts.ubuntu(
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
         ),
       ),
     );
