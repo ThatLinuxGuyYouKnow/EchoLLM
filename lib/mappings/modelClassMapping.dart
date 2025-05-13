b7bb84d9d1d398b296538a2123d33f01b6a83393
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-13 14:33:30 +0100

    chore: mapping for openai models

--- lib/mappings/modelClassMapping.dart
+++ lib/mappings/modelClassMapping.dart
@@ -2,5 +2,7 @@ Map<String, String> modelFamilyClass = {
   "gemini-2.5-pro": "gemini",
   "gemini-2.5-flash": "gemini",
   "gemini-2.0-flash": "gemini",
-  "gemini-2.0-flash-lite": "gemini"
+  "gemini-2.0-flash-lite": "gemini",
+  "gpt-4.1": "openai",
+  "gpt-4o": "openai"
 };
