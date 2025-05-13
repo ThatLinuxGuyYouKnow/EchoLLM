1850442a643832c8ba57911ada4686441201cb5a
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-13 14:58:26 +0100

    chore: mapping for x-ai models

--- lib/mappings/modelClassMapping.dart
+++ lib/mappings/modelClassMapping.dart
@@ -4,5 +4,8 @@ Map<String, String> modelFamilyClass = {
   "gemini-2.0-flash": "gemini",
   "gemini-2.0-flash-lite": "gemini",
   "gpt-4.1": "openai",
-  "gpt-4o": "openai"
+  "gpt-4o": "openai",
+  "grok-2-1212": "x-ai",
+  "grok-3-beta": "x-ai",
+  "grok-3-mini-beta": "x-ai"
 };
--- lib/mappings/modelSlugMappings.dart
+++ lib/mappings/modelSlugMappings.dart
@@ -3,5 +3,9 @@ Map<String, String> onlineModels = {
   "Gemini 2.5 Flash": "gemini-2.5-flash",
   "Gemini 2.0 Flash": "gemini-2.0-flash",
   "Gemini 2.0 Flash Lite": "gemini-2.0-flash-lite",
-  "GPT 4.1": "gpt-4.1"
+  "GPT 4.1": "gpt-4.1",
+  "GPT 4o": "gtp-4.1",
+  "Grok 2": "grok-2-1212",
+  "Grok 3(beta)": "grok-3-beta",
+  "Grok 3 mini(beta)": "grok-3-mini-beta"
 };
