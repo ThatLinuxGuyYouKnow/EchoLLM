a7ef3ebf826e21c1b9d236ac5c5b2db873258865
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-14 17:29:22 +0100

    chore: better handling of null cases for model responses

--- lib/logic/geminiHelper.dart
+++ lib/logic/geminiHelper.dart
@@ -1,11 +1,23 @@
+import 'dart:convert';
+import 'dart:js_interop';
+
 import 'package:http/http.dart' as http;
 
 class Geminihelper {
   String modelSlug;
   String apiKey;
   Geminihelper({required this.modelSlug, required this.apiKey});
-  getResponse() {
-    return http.get(Uri.parse(
+  getResponse() async {
+    var response = await http.get(Uri.parse(
         'https://generativelanguage.googleapis.com/v1beta/models/${modelSlug}:generateContent?key=${apiKey}'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body) as Map<String, dynamic>;
+      String modelResponse =
+          data['candidates'][0]['content']['parts'][0]['text'];
+      if (modelResponse.length > 1) {
+        return modelResponse;
+      }
+    } else {}
   }
 }
