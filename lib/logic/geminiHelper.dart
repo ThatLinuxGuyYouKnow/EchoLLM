2a8d22ca7c06e3a97f9609fea42802b27eb327ed
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-14 13:18:54 +0100

    chore: gemini helper method

--- lib/logic/geminiHelper.dart
+++ lib/logic/geminiHelper.dart
@@ -1,7 +1,11 @@
 import 'package:http/http.dart' as http;
 
 class Geminihelper {
+  String modelSlug;
+  String apiKey;
+  Geminihelper({required this.modelSlug, required this.apiKey});
   getResponse() {
-    return http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+    return http.get(Uri.parse(
+        'https://generativelanguage.googleapis.com/v1beta/models/${modelSlug}:generateContent?key=${apiKey}'));
   }
 }
