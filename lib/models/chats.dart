9f5686aecb936dacde51249de5f298a9cc1a05ba
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-19 23:44:11 +0100

    chore: generated Hive repo for Chats

--- lib/models/chats.dart
+++ lib/models/chats.dart
@@ -1,6 +1,36 @@
-class Chat {
+import 'package:hive/hive.dart';
+
+part 'chat.g.dart'; // This file will be generated
+
+@HiveType(typeId: 0)
+class Chat extends HiveObject {
+  @HiveField(0)
   late String id;
-  late String chatTile;
+
+  @HiveField(1)
+  late String chatTitle;
+
+  @HiveField(2)
   late DateTime lastModified;
-  late List messages;
+
+  @HiveField(3)
+  late List<Message> messages;
+}
+
+@HiveType(typeId: 1)
+class Message {
+  @HiveField(0)
+  late String id;
+
+  @HiveField(1)
+  late String content;
+
+  @HiveField(2)
+  late String role;
+
+  @HiveField(3)
+  late DateTime timestamp;
+
+  @HiveField(4)
+  Map<String, dynamic>? metadata;
 }
