79e346c21132e6f1c9f1d4fcfa8516613ec9a4ec
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-20 01:46:32 +0100

    chore: function for saving chats

---
+++ lib/dataHandlers/chatStrorage.dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+
+import 'package:echo_llm/models/chats.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+saveChatLocally(
+    {required List<Message> messages, required String chatTitle}) async {
+  final dateTimeRighNow = DateTime.now();
+  final randomPart = List.generate(3, (_) => Random().nextInt(1000)).join('');
+  final chatID = dateTimeRighNow.toString() + randomPart;
+  final chat = Chat()
+    ..chatTitle = chatTitle
+    ..id = chatID
+    ..lastModified = dateTimeRighNow
+    ..messages = messages;
+  final chatBox = await Hive.openBox<Chat>('chats');
+  chatBox.put(chat.id, chat);
+}
