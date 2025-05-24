562a0dde9d6522733b30c665d070a9bf56d3688f
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-24 15:44:54 +0100

    feature: button/tile for starting mew chat in sidebar

--- lib/widgets/sidebar.dart
+++ lib/widgets/sidebar.dart
@@ -151,6 +151,64 @@ class _SpecialDrawerTileState extends State<SpecialDrawerTile> {
   }
 }
 
+class NewChatTile extends StatefulWidget {
+  NewChatTile({super.key, required this.onTilePressed});
+
+  final Function() onTilePressed;
+
+  @override
+  State<NewChatTile> createState() => _NewChatTileState();
+}
+
+class _NewChatTileState extends State<NewChatTile> {
+  bool isHovered = false;
+
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: MouseRegion(
+        onHover: (event) {
+          isHovered = true;
+          setState(() {});
+        },
+        onExit: (event) {
+          isHovered = false;
+          setState(() {});
+        },
+        child: GestureDetector(
+          onTap: widget.onTilePressed,
+          child: Container(
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(
+                isHovered ? 0.7 : 0.4,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.add, color: Colors.white),
+                  Text(
+                    'New Chat',
+                    style: GoogleFonts.ubuntu(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class CustomSideBar extends StatefulWidget {
   const CustomSideBar({super.key});
 
@@ -178,7 +236,7 @@ class _CustomSideBarState extends State<CustomSideBar> {
   Widget build(BuildContext context) {
     final screenState = Provider.of<Screenstate>(context);
     final isOnChatScreen = screenState.isOnMainScreen;
-
+    final messageState = Provider.of<Messagestreamstate>(context);
     return Material(
       child: AnimatedContainer(
         color: const Color(0xFF1E2733),
@@ -188,6 +246,9 @@ class _CustomSideBarState extends State<CustomSideBar> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              NewChatTile(
+                onTilePressed: messageState.clear(),
+              ),
               DrawerTile(
                 tileTitle: 'Settings',
                 tileIcon: Icons.precision_manufacturing,
