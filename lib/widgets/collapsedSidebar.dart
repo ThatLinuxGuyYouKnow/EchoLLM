5e9ad7f1a43012639c32db05b336f6a6146f1430
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-26 00:05:39 +0100

    feature: collaped sidebar

---
+++ lib/widgets/collapsedSidebar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Collapsedsidebar extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(seconds: 1),
+      child: Column(),
+    );
+  }
+}
