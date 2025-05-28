ef8f5776703966f4f97b2205aecf3bee3d69a70a
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-28 23:25:43 +0100

    fix: switched to using onSubmitted in textfield instead of Focus listener

--- lib/widgets/textfield.dart
+++ lib/widgets/textfield.dart
@@ -25,8 +25,9 @@ class ChatTextField extends StatelessWidget {
         context: context, conversationHistory: messageState.messages);
     final existingID = messageState.chatID;
     final _shouldSendOnEnter = Provider.of<CONFIG>(context).shouldSendOnEnter;
-    final FocusNode keyboardNode = FocusNode();
-
+    final FocusNode textFieldFocusNode = FocusNode();
+    print(
+        'send on enter' + _shouldSendOnEnter.toString()); // this prints as true
     Future<void> sendMessage() async {
       final userMessage = chatController.text.trim();
       if (userMessage.isEmpty) return;
@@ -63,110 +64,106 @@ class ChatTextField extends StatelessWidget {
       }
     }
 
-    return Focus(
-      onKey: (node, event) {
-        if (event.logicalKey == LogicalKeyboardKey.enter &&
-            event is KeyDownEvent) {
-          if (chatController.text.isNotEmpty && _shouldSendOnEnter) {
-            sendMessage();
-            return KeyEventResult.handled;
-          }
-        }
-        return KeyEventResult.ignored;
-      },
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: AnimatedSize(
-          duration: const Duration(milliseconds: 100),
-          child: Container(
-            height: textfieldState.isExpanded ? 400 : 150,
-            constraints: BoxConstraints(
-                minHeight: 150, maxHeight: isExpanded ? 400 : 200),
-            width: isPhoneScreen ? 800 : screenWidth / 2.5,
-            child: Stack(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                                color: isExpanded
-                                    ? Colors.cyan
-                                    : Colors.transparent),
-                            color: const Color(0xFF1E2733),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Stack(
-                            children: [
-                              Positioned.fill(
-                                child: Focus(
-                                  onFocusChange: (isFocused) {
-                                    if (textfieldState.isExpanded &&
-                                        !isFocused) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: AnimatedSize(
+        duration: const Duration(milliseconds: 100),
+        child: Container(
+          height: textfieldState.isExpanded ? 400 : 150,
+          constraints:
+              BoxConstraints(minHeight: 150, maxHeight: isExpanded ? 400 : 200),
+          width: isPhoneScreen ? 800 : screenWidth / 2.5,
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: isExpanded
+                                  ? Colors.cyan
+                                  : Colors.transparent),
+                          color: const Color(0xFF1E2733),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Stack(
+                          children: [
+                            Positioned.fill(
+                              child: Focus(
+                                focusNode: textFieldFocusNode,
+                                onFocusChange: (isFocused) {
+                                  if (textfieldState.isExpanded && !isFocused) {
+                                    textfieldState.minimize();
+                                  }
+                                },
+                                autofocus: true,
+                                child: TextField(
+                                  textInputAction: _shouldSendOnEnter
+                                      ? TextInputAction.send
+                                      : TextInputAction.newline,
+                                  onSubmitted: (string) {
+                                    if (_shouldSendOnEnter) {
+                                      sendMessage();
+                                    } else {}
+                                  },
+                                  maxLines: null,
+                                  minLines: null,
+                                  expands: isExpanded,
+                                  scrollPadding:
+                                      EdgeInsets.only(top: isExpanded ? 20 : 0),
+                                  controller: chatController,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                      color: Colors.white,
+                                      fontSize: 15),
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: 'Type a message',
+                                    contentPadding: const EdgeInsets.only(
+                                        left: 12.0,
+                                        right: 40,
+                                        bottom: 10,
+                                        top: 10),
+                                    hintStyle: const TextStyle(
+                                        fontWeight: FontWeight.normal),
+                                  ),
+                                  onChanged: (text) {
+                                    if (text.length > 200 && !isExpanded) {
+                                      textfieldState.expand();
+                                    } else if (text.length < 50 &&
+                                        textfieldState.isExpanded) {
                                       textfieldState.minimize();
                                     }
                                   },
-                                  autofocus: true,
-                                  child: TextField(
-                                    maxLines: null,
-                                    minLines: null,
-                                    expands: isExpanded,
-                                    scrollPadding: EdgeInsets.only(
-                                        top: isExpanded ? 20 : 0),
-                                    controller: chatController,
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.w700,
-                                        color: Colors.white,
-                                        fontSize: 15),
-                                    decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText: 'Type a message',
-                                      contentPadding: const EdgeInsets.only(
-                                          left: 12.0,
-                                          right: 40,
-                                          bottom: 10,
-                                          top: 10),
-                                      hintStyle: const TextStyle(
-                                          fontWeight: FontWeight.normal),
-                                    ),
-                                    onChanged: (text) {
-                                      if (text.length > 200 && !isExpanded) {
-                                        textfieldState.expand();
-                                      } else if (text.length < 50 &&
-                                          textfieldState.isExpanded) {
-                                        textfieldState.minimize();
-                                      }
-                                    },
-                                  ),
                                 ),
                               ),
-                              Positioned(
-                                bottom: 4,
-                                right: 4,
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: const Color(0xFF1E2733),
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  child: ChatButton(
-                                    whenPressed: sendMessage,
-                                  ),
+                            ),
+                            Positioned(
+                              bottom: 4,
+                              right: 4,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFF1E2733),
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: ChatButton(
+                                  whenPressed: sendMessage,
                                 ),
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
