2271cd33319a6cc13b6d7796bc6c4a4b90965c4b
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-05-28 21:02:25 +0100

    feature: implemented send message on enter button presses

--- lib/widgets/textfield.dart
+++ lib/widgets/textfield.dart
@@ -6,7 +6,7 @@ import 'package:echo_llm/state_management/textfieldState.dart';
 import 'package:echo_llm/widgets/buttons.dart';
 import 'package:echo_llm/widgets/toastMessage.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 class ChatTextField extends StatelessWidget {
@@ -23,140 +23,148 @@ class ChatTextField extends StatelessWidget {
     final modelInference = InferenceSuperClass(
         context: context, conversationHistory: messageState.messages);
     final existingID = messageState.chatID;
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: AnimatedSize(
-        duration: const Duration(milliseconds: 100),
-        child: Container(
-          height: textfieldState.isExpanded ? 400 : 150,
-          constraints:
-              BoxConstraints(minHeight: 150, maxHeight: isExpanded ? 400 : 200),
-          width: isPhoneScreen ? 800 : screenWidth / 2.5,
-          child: Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                              color: isExpanded
-                                  ? Colors.cyan
-                                  : Colors.transparent),
-                          color: const Color(0xFF1E2733),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Stack(
-                          children: [
-                            Positioned.fill(
-                              child: Focus(
-                                onFocusChange: (isFocused) {
-                                  if (textfieldState.isExpanded && !isFocused) {
-                                    textfieldState.minimize();
-                                  }
-                                },
-                                autofocus: true,
-                                child: TextField(
-                                  maxLines: null,
-                                  minLines: null,
-                                  expands: isExpanded,
-                                  scrollPadding:
-                                      EdgeInsets.only(top: isExpanded ? 20 : 0),
-                                  controller: chatController,
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.w700,
-                                      color: Colors.white,
-                                      fontSize: 15),
-                                  decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: 'Type a message',
-                                    contentPadding: const EdgeInsets.only(
-                                        left: 12.0,
-                                        right: 40,
-                                        bottom: 10,
-                                        top: 10),
-                                    hintStyle: const TextStyle(
-                                        fontWeight: FontWeight.normal),
-                                  ),
-                                  onChanged: (text) {
-                                    if (text.length > 200 && !isExpanded) {
-                                      textfieldState.expand();
-                                    } else if (text.length < 50 &&
-                                        textfieldState.isExpanded) {
+    final FocusNode keyboardNode = FocusNode();
+
+    Future<void> sendMessage() async {
+      final userMessage = chatController.text.trim();
+      if (userMessage.isEmpty) return;
+
+      final messageState =
+          Provider.of<Messagestreamstate>(context, listen: false);
+      messageState.addMessage(message: userMessage);
+      messageState.setProcessing(true);
+
+      try {
+        final response = await modelInference.runInference(userMessage);
+        chatController.clear();
+
+        if (response != null && response.isNotEmpty) {
+          messageState.addMessage(message: response);
+
+          final hiveReadyMessages =
+              convertIndexedMessagesToHive(messageState.messages);
+          final title = titleFromFirstMessage(messageState.messages);
+
+          final chatId = await saveChatLocally(
+              existingChatID: existingID,
+              context: context,
+              messages: hiveReadyMessages,
+              chatTitle: title);
+          if (existingID.isEmpty) {
+            messageState.setCurrentChatID(chatId);
+          }
+        }
+      } catch (e) {
+        showCustomToast(context, message: 'Error $e');
+      } finally {
+        messageState.setProcessing(false);
+      }
+    }
+
+    return Focus(
+      onKey: (node, event) {
+        if (event.logicalKey == LogicalKeyboardKey.enter &&
+            event is KeyDownEvent) {
+          if (chatController.text.isNotEmpty) {
+            sendMessage();
+            return KeyEventResult.handled;
+          }
+        }
+        return KeyEventResult.ignored;
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: AnimatedSize(
+          duration: const Duration(milliseconds: 100),
+          child: Container(
+            height: textfieldState.isExpanded ? 400 : 150,
+            constraints: BoxConstraints(
+                minHeight: 150, maxHeight: isExpanded ? 400 : 200),
+            width: isPhoneScreen ? 800 : screenWidth / 2.5,
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                color: isExpanded
+                                    ? Colors.cyan
+                                    : Colors.transparent),
+                            color: const Color(0xFF1E2733),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Stack(
+                            children: [
+                              Positioned.fill(
+                                child: Focus(
+                                  onFocusChange: (isFocused) {
+                                    if (textfieldState.isExpanded &&
+                                        !isFocused) {
                                       textfieldState.minimize();
                                     }
                                   },
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                              bottom: 4, // Reduced from 10
-                              right: 4, // Reduced from 10
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: const Color(0xFF1E2733),
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                                child: ChatButton(
-                                  whenPressed: () async {
-                                    final userMessage =
-                                        chatController.text.trim();
-                                    if (userMessage.isEmpty) return;
-
-                                    final messageState =
-                                        Provider.of<Messagestreamstate>(context,
-                                            listen: false);
-                                    messageState.addMessage(
-                                        message: userMessage);
-                                    messageState.setProcessing(true);
-
-                                    try {
-                                      final response = await modelInference
-                                          .runInference(userMessage);
-                                      chatController.clear();
-
-                                      if (response != null &&
-                                          response.isNotEmpty) {
-                                        messageState.addMessage(
-                                            message: response);
-
-                                        final hiveReadyMessages =
-                                            convertIndexedMessagesToHive(
-                                                messageState.messages);
-                                        final title = titleFromFirstMessage(
-                                            messageState.messages);
-
-                                        final chatId = await saveChatLocally(
-                                            existingChatID: existingID,
-                                            context: context,
-                                            messages: hiveReadyMessages,
-                                            chatTitle: title);
-                                        if (existingID.isEmpty) {
-                                          messageState.setCurrentChatID(chatId);
-                                        }
+                                  autofocus: true,
+                                  child: TextField(
+                                    maxLines: null,
+                                    minLines: null,
+                                    expands: isExpanded,
+                                    scrollPadding: EdgeInsets.only(
+                                        top: isExpanded ? 20 : 0),
+                                    controller: chatController,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w700,
+                                        color: Colors.white,
+                                        fontSize: 15),
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: 'Type a message',
+                                      contentPadding: const EdgeInsets.only(
+                                          left: 12.0,
+                                          right: 40,
+                                          bottom: 10,
+                                          top: 10),
+                                      hintStyle: const TextStyle(
+                                          fontWeight: FontWeight.normal),
+                                    ),
+                                    onChanged: (text) {
+                                      if (text.length > 200 && !isExpanded) {
+                                        textfieldState.expand();
+                                      } else if (text.length < 50 &&
+                                          textfieldState.isExpanded) {
+                                        textfieldState.minimize();
                                       }
-                                    } catch (e) {
-                                      showCustomToast(context,
-                                          message: 'Error $e');
-                                    } finally {
-                                      messageState.setProcessing(false);
-                                    }
-                                  },
+                                    },
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                              Positioned(
+                                bottom: 4,
+                                right: 4,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: const Color(0xFF1E2733),
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  child: ChatButton(
+                                    whenPressed: sendMessage,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
