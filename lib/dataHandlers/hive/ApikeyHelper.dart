edf1a546fd748fa16ff621e18905d79c3e7a71f1
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-06-01 22:48:10 +0100

    refactor: moved inference methods to separate dir

---
+++ lib/dataHandlers/hive/ApikeyHelper.dart
@@ -0,0 +1,36 @@
+import 'package:echo_llm/mappings/modelAvailabilityMapping.dart';
+
+import 'package:get_storage/get_storage.dart';
+
+class ApiKeyHelper {
+  final box = GetStorage('api-keys');
+  storeKey({required String modelSlugNotName, required String apiKey}) async {
+    try {
+      await box.write(modelSlugNotName, apiKey);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  String readKey({required String modelSlugNotName}) {
+    final String key = box.read(modelSlugNotName) ?? '';
+
+    return key;
+  }
+
+  Map<String, String> getAvailableModelKeyMap() {
+    final result = <String, String>{};
+
+    onlineModelAvailability.forEach((modelSlug, isAvailable) {
+      if (isAvailable) {
+        final key = readKey(modelSlugNotName: modelSlug);
+        if (key.isNotEmpty) {
+          result[modelSlug] = key;
+        }
+      }
+    });
+
+    return result;
+  }
+}
