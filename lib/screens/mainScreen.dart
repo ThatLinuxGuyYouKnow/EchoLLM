e6155f21ac4924dd42172042ec2f011204939440
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-06-06 00:12:24 +0100

    chore: resized first time dialog

--- lib/screens/mainScreen.dart
+++ lib/screens/mainScreen.dart
@@ -3,6 +3,7 @@ import 'package:echo_llm/state_management/sidebarState.dart';
 import 'package:echo_llm/widgets/appBar.dart';
 import 'package:echo_llm/widgets/collapsedSidebar.dart';
 import 'package:echo_llm/widgets/modals/firstTimeUserModal.dart';
+
 import 'package:echo_llm/widgets/sidebar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -62,55 +63,4 @@ class _MainScreenState extends State<MainScreen> {
       ],
     );
   }
-}
-
-Widget buildFirstTimeUserPrompt() {
-  return Dialog(
-    backgroundColor: const Color(0xFF1E2733),
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(12),
-      side: BorderSide(color: Colors.blue.withOpacity(0.3), width: 1),
-    ),
-    child: Container(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'First Time?',
-            style: GoogleFonts.ubuntu(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          const SizedBox(height: 15),
-          Text(
-            "You'll need to enter at least one API key to get started with EchoLLM",
-            style: GoogleFonts.ubuntu(
-              color: Colors.grey[400],
-              fontSize: 15,
-            ),
-          ),
-          const SizedBox(height: 25),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              TextButton(
-                onPressed: () => null,
-                child: Text(
-                  'Got it!',
-                  style: GoogleFonts.ubuntu(
-                    color: Colors.cyanAccent[100],
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
-    ),
-  );
 }
--- lib/widgets/modals/firstTimeUserModal.dart
+++ lib/widgets/modals/firstTimeUserModal.dart
@@ -8,13 +8,48 @@ Widget buildFirstTimeUserPrompt() {
       borderRadius: BorderRadius.circular(12),
       side: BorderSide(color: Colors.blue.withOpacity(0.3), width: 1),
     ),
-    child: ListTile(
-      title: Text(
-        'First Time ?',
-        style: GoogleFonts.ubuntu(color: Colors.white),
+    child: Container(
+      padding: const EdgeInsets.all(20),
+      height: 200,
+      width: 500,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'First Time',
+            style: GoogleFonts.ubuntu(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(height: 15),
+          Text(
+            "You'll need to enter at least one API key to get started with EchoLLM",
+            style: GoogleFonts.ubuntu(
+              color: Colors.grey[400],
+              fontSize: 15,
+            ),
+          ),
+          const SizedBox(height: 25),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () => null,
+                child: Text(
+                  'Got it!',
+                  style: GoogleFonts.ubuntu(
+                    color: Colors.cyanAccent[100],
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
-      subtitle: Text(
-          "You'll need to enter at least one API key to get started with EchoLLM"),
     ),
   );
 }
