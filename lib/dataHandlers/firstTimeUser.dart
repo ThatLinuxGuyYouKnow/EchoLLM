adb9b27e0c3560cbe243a1426131e86cbcd19b25
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-06-06 00:03:02 +0100

    chor: method for storing user first time entry

--- lib/dataHandlers/firstTimeUser.dart
+++ lib/dataHandlers/firstTimeUser.dart
@@ -1,6 +1,8 @@
 import 'package:echo_llm/models/chats.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 storeUserFirstTimeEntry() async {
-  final chatBox = await Hive.openBox<Chat>('chats');
+  final box = GetStorage('preferences');
+  box.write('isUserNew', false);
 }
