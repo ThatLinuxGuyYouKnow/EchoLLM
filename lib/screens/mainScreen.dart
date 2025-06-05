df8b1eda2593487e6ac83aeda93d96326a2a1da8
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-06-06 00:07:37 +0100

    chore: moved first time user dialog to PostFrame build

--- lib/screens/mainScreen.dart
+++ lib/screens/mainScreen.dart
@@ -1,14 +1,11 @@
 import 'package:echo_llm/state_management/screenState.dart';
 import 'package:echo_llm/state_management/sidebarState.dart';
-
 import 'package:echo_llm/widgets/appBar.dart';
 import 'package:echo_llm/widgets/collapsedSidebar.dart';
 import 'package:echo_llm/widgets/modals/firstTimeUserModal.dart';
-
 import 'package:echo_llm/widgets/sidebar.dart';
-
 import 'package:flutter/material.dart';
-import 'package:get/utils.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class MainScreen extends StatefulWidget {
@@ -22,12 +19,23 @@ class _MainScreenState extends State<MainScreen> {
   final TextEditingController rawChat = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => _showFirstTimeDialog());
+  }
+
+  void _showFirstTimeDialog() {
     if (mounted) {
       showDialog(
-          context: context,
-          builder: (BuildContext context) => buildFirstTimeUserPrompt());
+        context: context,
+        builder: (BuildContext context) => buildFirstTimeUserPrompt(),
+      );
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final sidebarIsCollapsed =
         Provider.of<Sidebarstate>(context, listen: true).isCollapsed;
 
@@ -54,4 +62,55 @@ class _MainScreenState extends State<MainScreen> {
       ],
     );
   }
+}
+
+Widget buildFirstTimeUserPrompt() {
+  return Dialog(
+    backgroundColor: const Color(0xFF1E2733),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(12),
+      side: BorderSide(color: Colors.blue.withOpacity(0.3), width: 1),
+    ),
+    child: Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'First Time?',
+            style: GoogleFonts.ubuntu(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(height: 15),
+          Text(
+            "You'll need to enter at least one API key to get started with EchoLLM",
+            style: GoogleFonts.ubuntu(
+              color: Colors.grey[400],
+              fontSize: 15,
+            ),
+          ),
+          const SizedBox(height: 25),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () => null,
+                child: Text(
+                  'Got it!',
+                  style: GoogleFonts.ubuntu(
+                    color: Colors.cyanAccent[100],
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    ),
+  );
 }
