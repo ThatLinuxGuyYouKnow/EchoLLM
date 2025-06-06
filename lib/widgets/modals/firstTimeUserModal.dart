56f8a8686ed65c844f59a8be9e29e06b5c061b69
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-06-06 03:19:49 +0100

    feature: new user UX improvements

--- lib/widgets/modals/firstTimeUserModal.dart
+++ lib/widgets/modals/firstTimeUserModal.dart
@@ -11,68 +11,70 @@ Widget buildFirstTimeUserPrompt(
       side: BorderSide(color: Colors.blue.withOpacity(0.3), width: 1),
     ),
     child: Container(
-      padding: const EdgeInsets.all(20),
+      padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 20),
       height: 190,
       width: 500,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'First Time?',
-            style: GoogleFonts.ubuntu(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          const SizedBox(height: 15),
-          Text(
-            "You'll need to enter at least one API key to get started with EchoLLM",
-            style: GoogleFonts.ubuntu(
-              color: Colors.grey[400],
-              fontSize: 15,
-            ),
-          ),
-          const SizedBox(height: 25),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              TextButton(
-                onPressed: () {
-                  onNegativeButtonPressed();
-                },
-                child: Text(
-                  "I'll do it later",
-                  style: GoogleFonts.ubuntu(
-                    color: Colors.grey[400],
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'First Time?',
+              style: GoogleFonts.ubuntu(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
               ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF4C83D1),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                ),
-                onPressed: () {
-                  onPositiveButtonPressed();
-                },
-                child: Text(
-                  'Got it!',
-                  style: GoogleFonts.ubuntu(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
+            ),
+            const SizedBox(height: 15),
+            Text(
+              "You'll need to enter at least one API key to get started with EchoLLM",
+              style: GoogleFonts.ubuntu(
+                color: Colors.grey[400],
+                fontSize: 15,
               ),
-            ],
-          )
-        ],
+            ),
+            const SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    onNegativeButtonPressed();
+                  },
+                  child: Text(
+                    "I'll do it later",
+                    style: GoogleFonts.ubuntu(
+                      color: Colors.grey[400],
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF4C83D1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24, vertical: 12),
+                  ),
+                  onPressed: () {
+                    onPositiveButtonPressed();
+                  },
+                  child: Text(
+                    'Got it!',
+                    style: GoogleFonts.ubuntu(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     ),
   );
