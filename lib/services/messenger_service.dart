cdb65375b02f83ab32ebf4ce05195142866dafab
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-06-15 11:07:59 +0100

    chore: services directory renamed

--- lib/services/messenger_service.dart
+++ lib/services/messenger_service.dart
@@ -1,10 +1,8 @@
 import 'package:echo_llm/widgets/toastMessage.dart';
 import 'package:flutter/material.dart';
 
-// 1. Create the GlobalKey
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-// 2. Create a class to manage showing your toast
 class MessengerService {
   void showToast(String message,
       {ToastMessageType type = ToastMessageType.passive}) {
