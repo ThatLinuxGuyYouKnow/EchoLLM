293d30dc61e815ecdeda1b0e56a94f2d52cd363f
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-06-18 00:31:48 +0100

    feature: implemented anthropic headers into helper logic

--- lib/inference/cluadeHelper.dart
+++ lib/inference/cluadeHelper.dart
@@ -1,6 +1,34 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
-class Cluadehelper {
+class Claudehelper {
   final String modelSlug;
   final String apiKey;
+  Claudehelper({required this.modelSlug, required this.apiKey});
+
+  getResponse(
+      {required String userPrmopt,
+      required List<Map<String, String>> messageHistory}) async {
+    final messages = [
+      ...messageHistory.map((entry) => {
+            'role': entry['role'],
+            'content': entry['content'],
+          }),
+      {'role': 'user', 'content': userPrmopt}
+    ];
+
+    final response = await http.post(
+      Uri.parse('https://api.openai.com/v1/chat/completions'),
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': '$apiKey',
+      },
+      body: jsonEncode({
+        'model': modelSlug,
+        'messages': messages,
+      }),
+    );
+    return _handleResponse(response);
+  }
 }
