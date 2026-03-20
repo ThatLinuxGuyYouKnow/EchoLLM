7cb7037c1de451892ee540e29cbb7f8e0e0ee699
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2026-03-20 13:54:28 +0100

    feat: open window maximized and rename title to EchoLLM

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -40,14 +40,15 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "echo_llm");
+    gtk_header_bar_set_title(header_bar, "EchoLLM");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "echo_llm");
+    gtk_window_set_title(window, "EchoLLM");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
+  gtk_window_maximize(window);
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
