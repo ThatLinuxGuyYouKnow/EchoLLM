b9a209e40536983fb641ebd6d7be00ee627673b2
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2026-03-26 02:00:59 +0100

    Remove vosk_flutter from platform plugin registrants

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,7 +5,6 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   permission_handler_windows
   url_launcher_windows
-  vosk_flutter
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
@@ -16,7 +15,7 @@ set(PLUGIN_BUNDLED_LIBRARIES)
 foreach(plugin ${FLUTTER_PLUGIN_LIST})
   add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/windows plugins/${plugin})
   target_link_libraries(${BINARY_NAME} PRIVATE ${plugin}_plugin)
-  list(APPEND PLUGIN_BUNDLED_LIBRARIES $<TARGET_FILE:${plugin}_plugin>)
+  list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${plugin}_bundled_libraries})
   list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${plugin}_bundled_libraries})
 endforeach(plugin)
 
